8bf85f74be27ecfab438e04cd3b4dd99f2ec4b32
gonzad77 <startapplabs@StartApps-MacBook-Pro.local>
2016-11-17 16:55:28 -0300

    Se muestra el mapa

---
+++ src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { IonicApp, IonicModule } from 'ionic-angular';
+import { MyApp } from './app.component';
+import { MapPage } from '../pages/map/map';
+
+@NgModule({
+  declarations: [
+    MyApp,
+    MapPage
+  ],
+  imports: [
+    IonicModule.forRoot(MyApp)
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    MapPage
+  ],
+  providers: []
+})
+export class AppModule {}
